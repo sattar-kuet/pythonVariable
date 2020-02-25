7f2b29a5e88d14a26e57fd8132a1dd9246607b56
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-26 00:02:17 +0600

    first commit

---
+++ variable.py
@@ -0,0 +1,23 @@
+number = 10
+number = 25
+number2 = 45
+new_number = number - 10
+new_number = number - number2
+new_number = number*number2
+new_number = number/10
+
+a = 10
+b = 2
+c = a%b
+d = 10%3
+print(number)
+print(new_number)
+print(c)
+print(d)
+x = 10
+y = 10 - 10*x
+z = (10 - 10)*x
+
+print(y)
+print(z)
+
